67ef1b17e773d77da6cf90eab2fed56152e22977
Zbigniew Romanowski <romz@wp.pl>
2019-04-06 20:36:16 +0200

    Set correct flag in cmake for production

--- cmake/romz_macros.cmake
+++ cmake/romz_macros.cmake
@@ -9,8 +9,6 @@
 option( ROMZ_PRODUCTION_ENABLED "Enable some performace optimization defining PRODUCTION" OFF )
 if( ROMZ_PRODUCTION_ENABLED )
     list( APPEND ROMZ_COMPILER_FLAGS -DPRODUCTION )
-else()
-    list( APPEND ROMZ_COMPILER_FLAGS -DPRODUCTION )
 endif()
 
 
